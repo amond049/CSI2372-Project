18baaa3d2694dbe7548eb446791d299768b412af
Pradyu <pvasu009@uottawa.ca>
2024-11-18 16:54:44 -0500

    overrode << to satisfy Virtual Friend Function Idiom

--- Card.h
+++ Card.h
@@ -10,6 +10,12 @@ class Card {
 		virtual int getCardsperCoin(int coins) const = 0;
 		virtual string getName() const = 0;
 		virtual void print(ostream& out) const = 0;
+
+		 // Global stream insertion operator using the idiom mentioned in the instructions (operator override)
+    friend std::ostream& operator<<(std::ostream& out, const Card& card) {
+        card.print(out); // call the virtual print() function
+        return out;
+    }
 };
 
 
